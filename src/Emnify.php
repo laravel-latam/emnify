8454da81ac4a4de57cf92c9a1a38d2b8f9f702cc
NeftaliYagua <NeftaliYagua@users.noreply.github.com>
2023-07-30 01:18:56 +0000

    Fix styling

--- src/Emnify.php
+++ src/Emnify.php
@@ -12,14 +12,13 @@ class Emnify
 {
     protected $config;
 
-
-    public function endpoints($q=null, $sort=null, $page=null, $per_page=null)
+    public function endpoints($q = null, $sort = null, $page = null, $per_page = null)
     {
-        
+
         $token = Storage::disk('local')->get('access_token');
         $this->config = Configuration::getDefaultConfiguration()
             ->setAccessToken($token);
-            
+
         $apiInstance = new EndpointApi(
             new Client(),
             $this->config
@@ -27,6 +26,7 @@ class Emnify
 
         try {
             $result = $apiInstance->getEndpoints($q, $sort, $page, $per_page);
+
             return collect($result);
         } catch (Exception $e) {
             throw new Exception('Exception when calling EndpointApi->getEndpoints: ', $e->getMessage());
